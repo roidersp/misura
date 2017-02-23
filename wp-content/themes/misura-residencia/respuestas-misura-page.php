d18f5e8ab8ca1a1aee0682dce90c6d0bd6c017f4
Rodrigo Luis Santander <roidersp@gmail.com>
2017-02-23 12:35:57 -0600

    inicio respuesta y back de base de datos

---
+++ wp-content/themes/misura-residencia/respuestas-misura-page.php
@@ -0,0 +1,45 @@
+<?php
+/*
+Template Name: Te tratamos bien
+*/
+?>
+<?php get_header(); ?>
+<main id="tratamos">
+  <div class="container">
+    <div class="tratamos_header">
+      <img src="<?= get_template_directory_uri(); ?>/assets/images/Te-tratamos-title.png" alt="Te-tratamos-title"  />
+      <div style="clear: both"></div>
+      <span>
+        Activa todos tus sentidos, un nuevo mundo te espera.<br>
+Hay un nuevo “bienestar” en el aire: El bienestar Misura que te consciente y te trata bien
+      </span>
+    </div>
+    <div class="tratamos_contenido">
+      <?php
+        // check if the repeater field has rows of data
+        if( have_rows('contenido') ):
+         	// loop through the rows of data
+            while ( have_rows('contenido') ) : the_row();
+            ?>
+             <div class="row">
+               <div class="col s12 m6">
+                 <img src="<?= get_sub_field( "imagen_izq" )["url"] ?>" alt="<?= get_sub_field( "imagen_izq" )["alt"] ?>" />
+               </div>
+               <div class="col s12 m6">
+                 <img src="<?= get_sub_field( "imagen_der" )["url"] ?>" alt="<?= get_sub_field( "imagen_der" )["alt"] ?>" />
+               </div>
+             </div>
+        <?php
+            endwhile;
+        
+        else :
+        
+            // no rows found
+        
+        endif;
+        
+        ?>
+    </div>
+  </div>
+</main>
+<?php get_footer(); ?>
